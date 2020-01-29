9f1e0a16cc124235d7867b5a431d31771ab7c3ba
guangsheng.li01 <guangsheng.li01@liulishuo.com>
2020-01-29 13:31:02 +0800

    .

---
+++ src/a0000_sample.rs
@@ -0,0 +1,17 @@
+struct Solution {}
+
+impl Solution {
+    fn add(a: i32, b: i32) -> i32 {
+        a + b
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_case0() {
+        assert_eq!(5, Solution::add(2, 3));
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,6 @@
+mod a0000_sample;
+mod a0001_two_sum;
+
+fn main() {
+    println!("Hello, world!");
+}
